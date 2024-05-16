cad595114499be04a6704f23f4cfcd17ae1a91d9
Marc Jamilano <108918196+mjamilano24@users.noreply.github.com>
2024-05-16 01:11:20 -0700

    Setting up controllers
    Begin adding controllers

---
+++ controllers/usersController.js
@@ -0,0 +1,11 @@
+const User = require('../models/User')
+const User = require('../models/Note')
+const asyncHandler = require('express-async-handler')
+const bcrypt = require('bcrypt')
+
+// @desc Get all users
+// @route GET /users
+// @access Private
+const getAllUsers = (async (req, res) => {
+
+})
